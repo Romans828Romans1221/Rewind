f4439472172bfcd573244bcc6cfb0623dcd2cd9e
jse116 <54330144+jse116@users.noreply.github.com>
2022-08-24 13:14:42 -0500

    Initial Commit of REWIND

--- JSsiteTemplate/js/site.js
+++ JSsiteTemplate/js/site.js
@@ -1,5 +1,42 @@
-// Calll Hello World 
-function HelloWorld() {
-    alert("Hello World");
+//Get the Values from the page 
+//controller function 
+function getValue(){
 
+    document.getElementById("alert").classList.add("invisible");
+    
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
+}
+
+//Reverse the string 
+// Logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //reverse a string using a for loop 
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+
+    return revString;
+
+
+}
+
+
+//Display the reversed string to the user
+//View Function 
+function displayString(revString){
+
+    //write the message to the page 
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //show the alert box 
+    document.getElementById("alert").classList.remove("invisible");
 }
